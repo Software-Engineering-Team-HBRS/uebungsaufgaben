3fa508203395de43933f41dda0d49f2dbfb37f14
Martin Braun <martinbraun1997wgmail.com>
2025-04-08 15:06:42 +0200

    test intellij ultimate

--- Uebung1/src/Main.java
+++ Uebung1/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Patrick");
-        System.out.println("Intellij Ultimate");
+        System.out.println("Intellij Ultimate Test");
 
 
 
